068cd277a571af1be119071822fc3e2d8c8c3460
random-71 <166395399+random-71@users.noreply.github.com>
2024-04-08 13:55:22 +0200

    Add files via upload

---
+++ ConsoleApp1/AsposeTransform.cs
@@ -0,0 +1,38 @@
+﻿using Aspose.Cells;
+using System.Globalization;
+using System.IO;
+
+namespace ConsoleApp1.AsposePdf
+{
+    public static class AsposeTransfrom
+    {
+
+        public static byte[] ExcelToPdf(string name)
+        {
+            try
+            {
+                Aspose.Cells.License license = new Aspose.Cells.License();
+                license.SetLicense(@"C:\Users\SE86826\OneDrive - Repsol\Documents\04 - Requerimientos\3161\Aspose.Total.NET.lic");
+
+                using (Stream data = new FileStream("PoS Support.xlsx", FileMode.Open, FileAccess.Read))
+                {
+                    
+                    data.Position = 0;
+
+                    using (MemoryStream dstStream = new MemoryStream())
+                    {
+                        var s = new Workbook(data);
+                        s.Save(@"C:\Users\SE86826\source\repos\ConsoleApp1\ConsoleApp1\bin\Debug\kk.pdf");
+                        s.Save(dstStream, SaveFormat.Pdf);
+                        return dstStream.ToArray();
+                    } 
+                }
+            }
+            catch
+            {
+                return new byte[0];
+            }
+        }
+
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,35 @@
+﻿using ConsoleApp1.AsposePdf;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Byte[] data = AsposeTransfrom.ExcelToPdf("");
+
+                File.WriteAllBytes("EndFile.pdf", data);
+
+                Console.WriteLine("Proccess finished");
+            } catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Press any Key...");
+                Console.ReadKey();
+            }
+
+        }
+    }
+}
